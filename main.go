e64e5fb16d53335c1172892366b46b8dd2a546d6
Peter Müller <peter7995@gmail.com>
2021-11-16 19:47:57 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Field struct {
+	name string
+	dest interface{}
+}
+
+func Fields(fields ...Field) (string, []interface{}) {
+	sqlString := ""
+	destinations := make([]interface{},0)
+	for _, field := range fields {
+		if sqlString != "" {
+			sqlString += ", "
+		}
+		sqlString += field.name
+		destinations = append(destinations, field.dest)
+		
+	}
+	return sqlString, destinations
+}
+
+func main() {
+
+	data := struct {
+		name string
+		surname  int
+	}{}
+
+	fields, destinations := Fields(
+		Field{"NAME", &data.name},
+		Field{"SURNAME", &data.surname},
+	)
+
+	query := fmt.Sprintf("SELECT %s FROM CATS", fields)
+	fmt.Println(query, destinations)
+}
